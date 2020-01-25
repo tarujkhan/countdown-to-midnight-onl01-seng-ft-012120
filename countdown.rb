03bb6a82d57b1074c6dcf1ed2857a37e80f5a5ab
taroj khan <tarujkhan@yahoo.com>
2020-01-25 19:01:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,12 +8,12 @@ def countdown(x)
   return "HAPPY NEW YEAR!"
 end
 
-def countown_with_sleep(x)
+def countdown_with_sleep(x)
   
   while x > 0 
    puts "#{x} SECOND(S)!"
     x -= 1
-   sleep 5
+   sleep 1
   end
   "HAPPY NEW YEAR!"
 end
